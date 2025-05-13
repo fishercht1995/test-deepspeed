72965b5f9ef23df8f7e64af00f8fd972694af25c
yuqi <jwx3px@virginia.edu>
2025-05-13 19:09:39 +0000

    update Pippy

--- test_pippy/run_workload.sh
+++ test_pippy/run_workload.sh
@@ -6,7 +6,7 @@ SCRIPTS=$(ls test_*.py | sort)
 for script in $SCRIPTS; do
   echo "▶ Running $script..." | tee -a ~/results.txt
   pdsh -w 44.192.252.191 "conda activate deepspeed-inference && cd /home/ubuntu/test-deepspeed/test_pippy && ./run_torch.sh 0 $script" >> ~/result.txt &
-  pdsh -w 3.91.112.200   "conda activate deepspeed-inference && cd /home/ubuntu/test-deepspeed/test_pippy && ./run_torch.sh 1 $script" >> ~/result.txt &
+  pdsh -w 18.207.132.213  "conda activate deepspeed-inference && cd /home/ubuntu/test-deepspeed/test_pippy && ./run_torch.sh 1 $script" >> ~/result.txt &
   wait
   sleep 10
 done
